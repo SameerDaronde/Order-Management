c11de0178b3712ca1c2d963d47c7c8f663f9f84b
vscode <D.sameerrrrrr94@outlook.com>
2025-01-16 17:23:04 +0530

    Added The service classes for login & signup

---
+++ login.js
@@ -0,0 +1,9 @@
+/**Add the Login Functionality*/
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON requests
+
+app.use(express.json());
---
+++ service.js
@@ -0,0 +1,9 @@
+console.log('Added new Functionality');
+
+// Import the required modules/**Added Login functionality */
+
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const app = express();
